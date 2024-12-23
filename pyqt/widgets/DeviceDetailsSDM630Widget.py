947dd5f1469992a211f886f080652256a4676e13
v1adem <vladem255@gmail.com>
2024-12-23 13:35:00 +0200

    feat: add tabs (but remove indicators)

--- pyqt/widgets/DeviceDetailsSDM630Widget.py
+++ pyqt/widgets/DeviceDetailsSDM630Widget.py
@@ -4,7 +4,7 @@ import pyqtgraph as pg
 import xlsxwriter
 from PyQt5.QtCore import Qt, QSortFilterProxyModel, QTime, QTimer, QDate
 from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem, QFont
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QMessageBox, QFileDialog, QTabWidget
 from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QLabel, QWidget, QSplitter, QTableView, QCalendarWidget, \
     QHBoxLayout, QDialog, QLCDNumber, QCheckBox, QDateEdit, QGridLayout
 from pyqtgraph import AxisItem
@@ -93,14 +93,36 @@ class DeviceDetailsSDM630Widget(QWidget):
         right_splitter.setChildrenCollapsible(False)
         main_splitter.addWidget(right_splitter)
 
-        top_right_widget = QWidget()
-        top_right_layout = QVBoxLayout(top_right_widget)
+        # Додання вкладок для 3-х фаз
+        self.tabs = QTabWidget()
+        self.tabs.setStyleSheet("font-size: 16px;")
 
+        # Створення вкладок для кожної фази
+        self.phase1_tab = QWidget()
+        self.phase2_tab = QWidget()
+        self.phase3_tab = QWidget()
+
+        # Додавання вкладок до табів
+        self.tabs.addTab(self.phase1_tab, "Фаза 1")
+        self.tabs.addTab(self.phase2_tab, "Фаза 2")
+        self.tabs.addTab(self.phase3_tab, "Фаза 3")
+
+        # Додаємо вкладки до правої частини спліттера
+        right_splitter.addWidget(self.tabs)
+
+        # Налаштування макетів для кожної фази
+        self.phase1_layout = QVBoxLayout(self.phase1_tab)
+        self.phase2_layout = QVBoxLayout(self.phase2_tab)
+        self.phase3_layout = QVBoxLayout(self.phase3_tab)
+
+        # Додати кнопку вибору діапазону на кожну вкладку
         self.select_range_button = QPushButton("Обрати проміжок")
         self.select_range_button.setStyleSheet("font-size: 16px;")
-        # self.select_range_button.clicked.connect(self.open_date_range_dialog)
-        top_right_layout.addWidget(self.select_range_button)
+        self.phase1_layout.addWidget(self.select_range_button)
+        self.phase2_layout.addWidget(self.select_range_button)
+        self.phase3_layout.addWidget(self.select_range_button)
 
+        # Створюємо графіки для кожної фази
         self.voltage_graph_widget = pg.PlotWidget()
         self.current_graph_widget = pg.PlotWidget()
         self.energy_graph_widget = pg.PlotWidget()
@@ -109,11 +131,9 @@ class DeviceDetailsSDM630Widget(QWidget):
         self.current_graph_widget.setAxisItems({'bottom': DateAxisItem(orientation='bottom')})
         self.energy_graph_widget.setAxisItems({'bottom': DateAxisItem(orientation='bottom')})
 
+        # Налаштування графіків для кожної фази
         self.voltage_curve = self.voltage_graph_widget.plot(pen=pg.mkPen(color='b', width=2), name="Напруга")
         self.current_curve = self.current_graph_widget.plot(pen=pg.mkPen(color='r', width=2), name="Струм")
-
-        # self.energy_bar_item = pg.BarGraphItem(width=5, height=5, brush='g', x=1)
-        # self.energy_graph_widget.addItem(self.energy_bar_item)
 
         # Для графіка напруги
         self.voltage_graph_widget.setLabel('left', 'Напруга', units='В')
@@ -124,85 +144,32 @@ class DeviceDetailsSDM630Widget(QWidget):
         # Для графіка споживання
         self.energy_graph_widget.setLabel('left', 'Споживання', units='кВт·год')
 
-        top_right_layout.addWidget(self.voltage_graph_widget)
-        top_right_layout.addWidget(self.current_graph_widget)
-        top_right_layout.addWidget(self.energy_graph_widget)
+        # Додаємо графіки до вкладки
+        self.phase1_layout.addWidget(self.voltage_graph_widget)
+        self.phase1_layout.addWidget(self.current_graph_widget)
+        self.phase1_layout.addWidget(self.energy_graph_widget)
 
+        # Повторюємо для інших фаз
+        self.phase2_layout.addWidget(self.voltage_graph_widget)
+        self.phase2_layout.addWidget(self.current_graph_widget)
+        self.phase2_layout.addWidget(self.energy_graph_widget)
+
+        self.phase3_layout.addWidget(self.voltage_graph_widget)
+        self.phase3_layout.addWidget(self.current_graph_widget)
+        self.phase3_layout.addWidget(self.energy_graph_widget)
+
+        # Додаємо автооновлення до кожної вкладки
         self.auto_update_checkbox = QCheckBox("Автооновлення")
         self.auto_update_checkbox.setChecked(True)  # За замовчуванням увімкнено
-        top_right_layout.addWidget(self.auto_update_checkbox)
+        self.phase1_layout.addWidget(self.auto_update_checkbox)
+        self.phase2_layout.addWidget(self.auto_update_checkbox)
+        self.phase3_layout.addWidget(self.auto_update_checkbox)
 
-        right_splitter.addWidget(top_right_widget)
-
-        bottom_right_widget = QWidget()
-        self.bottom_right_layout = QHBoxLayout(bottom_right_widget)
-
-        # Ліва частина: Годинник
-        self.left_layout = QVBoxLayout()
-
-        # Заголовок "Поточний час"
-        self.clock_title = QLabel("Поточний час")
-        self.clock_title.setStyleSheet("font-size: 16pt; font-weight: bold;")
-        self.clock_title.setAlignment(Qt.AlignCenter)  # Вирівнювання тексту по центру
-        self.left_layout.addWidget(self.clock_title)
-
-        # Годинник
-        self.clock_label = QLabel()
-        self.clock_label.setStyleSheet("font-size: 24pt;")  # Збільшений шрифт для годинника
-        self.clock_label.setAlignment(Qt.AlignCenter)  # Вирівнювання по центру
-        self.left_layout.addWidget(self.clock_label)
-
-        self.left_layout.addStretch()
-
-        # Права частина: Індикатори в сітці
-        self.grid_layout = QGridLayout()
-
-        # Індикатори
-        self.voltage_lcd = QLCDNumber()
-        self.voltage_lcd.setSegmentStyle(QLCDNumber.Flat)
-        self.current_lcd = QLCDNumber()
-        self.current_lcd.setSegmentStyle(QLCDNumber.Flat)
-        self.power_lcd = QLCDNumber()
-        self.power_lcd.setSegmentStyle(QLCDNumber.Flat)
-        self.energy_lcd = QLCDNumber()
-        self.energy_lcd.setSegmentStyle(QLCDNumber.Flat)
-
-        # Підписи
-        self.voltage_label = QLabel("Напруга (V)")
-        self.voltage_label.setStyleSheet("font-size: 14pt; font-weight: bold;")
-        self.current_label = QLabel("Струм (A)")
-        self.current_label.setStyleSheet("font-size: 14pt; font-weight: bold;")
-        self.power_label = QLabel("Потужність (W)")
-        self.power_label.setStyleSheet("font-size: 14pt; font-weight: bold;")
-        self.energy_label = QLabel("Спожито (kWh)")
-        self.energy_label.setStyleSheet("font-size: 14pt; font-weight: bold;")
-
-        # Додавання до сітки
-        self.grid_layout.addWidget(self.voltage_label, 0, 0)
-        self.grid_layout.addWidget(self.voltage_lcd, 1, 0)
-        self.grid_layout.addWidget(self.current_label, 0, 1)
-        self.grid_layout.addWidget(self.current_lcd, 1, 1)
-        self.grid_layout.addWidget(self.power_label, 2, 0)
-        self.grid_layout.addWidget(self.power_lcd, 3, 0)
-        self.grid_layout.addWidget(self.energy_label, 2, 1)
-        self.grid_layout.addWidget(self.energy_lcd, 3, 1)
-
-        # Додавання до головного компонувальника
-        self.bottom_right_layout.addLayout(self.left_layout)  # Ліва частина
-        self.bottom_right_layout.addLayout(self.grid_layout)  # Права частина (сітка)
-
-        right_splitter.addWidget(bottom_right_widget)
-
-        self.set_splitter_fixed_ratios(main_splitter, [1, 1])
-        self.set_splitter_fixed_ratios(right_splitter, [3, 1])
-
-        #self.timer = QTimer(self)
-        #self.timer.timeout.connect(self.update_ui)
-        #self.timer.setInterval(1000)
-        #self.timer.start()
+        # Налаштування відображення для правої частини
+        # self.set_splitter_fixed_ratios(main_splitter, [1, 1])
+        # self.set_splitter_fixed_ratios(right_splitter, [3, 1])
 
         self.load_report_data()
-        #self.update_graphs(self.start_date, self.end_date)
         self.set_light_theme()
 
     def set_light_theme(self):
@@ -243,19 +210,19 @@ class DeviceDetailsSDM630Widget(QWidget):
     def create_table_model(self, report_data, device):
         column_labels = {
             "timestamp": "Час",
-            "line_voltage_1": "Лінійна напруга (Фаза 1)",
-            "line_voltage_2": "Лінійна напруга (Фаза 2)",
-            "line_voltage_3": "Лінійна напруга (Фаза 3)",
-            "current_1": "Струм (Фаза 1)",
-            "current_2": "Струм (Фаза 2)",
-            "current_3": "Струм (Фаза 3)",
-            "power_1": "Активна потужність (Фаза 1)",
-            "power_2": "Активна потужність (Фаза 2)",
-            "power_3": "Активна потужність (Фаза 3)",
-            "power_factor_1": "Коефіцієнт потужності (Фаза 1)",
-            "power_factor_2": "Коефіцієнт потужності (Фаза 2)",
-            "power_factor_3": "Коефіцієнт потужності (Фаза 3)",
-            "total_system_power": "Загальна потужність системи",
+            "line_voltage_1": "Лінійна напруга\n(Фаза 1)\n",
+            "line_voltage_2": "Лінійна напруга\n(Фаза 2)\n",
+            "line_voltage_3": "Лінійна напруга\n(Фаза 3)\n",
+            "current_1": "Струм\n(Фаза 1)\n",
+            "current_2": "Струм\n(Фаза 2)\n",
+            "current_3": "Струм\n(Фаза 3)\n",
+            "power_1": "Активна потужність\n(Фаза 1)\n",
+            "power_2": "Активна потужність\n(Фаза 2)\n",
+            "power_3": "Активна потужність\n(Фаза 3)\n",
+            "power_factor_1": "Коефіцієнт потужності\n(Фаза 1)\n",
+            "power_factor_2": "Коефіцієнт потужності\n(Фаза 2)\n",
+            "power_factor_3": "Коефіцієнт потужності\n(Фаза 3)\n",
+            "total_system_power": "Загальна потужність\nсистеми",
             #"total_system_VA": "Загальна потужність",
             #"total_system_VAr": "Загальна реактивна потужність",
             #"total_system_power_factor": "Коефіцієнт потужності системи",
@@ -263,11 +230,11 @@ class DeviceDetailsSDM630Widget(QWidget):
             #"total_export_kwh": "Загальне споживання (експорт)",
             #"total_import_kVAh": "Загальне споживання (імпорт)",
             #"total_export_kVAh": "Загальне споживання (експорт)",
-            "total_kVAh": "Загальна енергія кВА·год",
-            "_1_to_2_voltage": "Напруга між Фазою 1 і Фазою 2",
-            "_2_to_3_voltage": "Напруга між Фазою 2 і Фазою 3",
-            "_3_to_1_voltage": "Напруга між Фазою 3 і Фазою 1",
-            "neutral_current": "Струм нейтралі",
+            "total_kVAh": "Загальна енергія\n",
+            "_1_to_2_voltage": "Напруга між\nФазою 1 і Фазою 2\n",
+            "_2_to_3_voltage": "Напруга між\nФазою 2 і Фазою 3\n",
+            "_3_to_1_voltage": "Напруга між\nФазою 3 і Фазою 1\n",
+            "neutral_current": "Струм нейтралі\n",
             #"line_voltage_THD_1": "THD лінійної напруги (Фаза 1)",
             #"line_voltage_THD_2": "THD лінійної напруги (Фаза 2)",
             #"line_voltage_THD_3": "THD лінійної напруги (Фаза 3)",
@@ -281,8 +248,8 @@ class DeviceDetailsSDM630Widget(QWidget):
             #"phase_voltage_THD_2": "THD фазної напруги (Фаза 2)",
             #"phase_voltage_THD_3": "THD фазної напруги (Фаза 3)",
             #"average_line_to_line_voltage_THD": "Середній THD лінійної напруги",
-            "total_kWh": "Загальна енергія кВт·год",
-            "total_kVArh": "Загальна реактивна енергія (кВАр·год)",
+            "total_kWh": "Загальна енергія\n",
+            "total_kVArh": "Загальна реактивна\nенергія\n",
             #"import_kWh_1": "Імпортована енергія (Фаза 1)",
             #"import_kWh_2": "Імпортована енергія (Фаза 2)",
             #"import_kWh_3": "Імпортована енергія (Фаза 3)",
@@ -307,11 +274,12 @@ class DeviceDetailsSDM630Widget(QWidget):
         register_map = RegisterMap.get_register_map(device.model)
         columns_with_units = RegisterMap.get_columns_with_units(register_map)
 
+        # Збираємо стовпці, що є в даних і в column_labels
         columns = set()
 
         for report in report_data:
             for column_name in report.__table__.columns.keys():
-                if column_name not in ['id', 'device_id'] and getattr(report, column_name) is not None:
+                if column_name in column_labels and getattr(report, column_name) is not None:
                     columns.add(column_name)
 
         columns = sorted(columns)
@@ -334,6 +302,13 @@ class DeviceDetailsSDM630Widget(QWidget):
         for row, report in enumerate(report_data):
             for col, column in enumerate(columns):
                 value = getattr(report, column)
+                if column == "timestamp" and value is not None:
+                    # Перетворюємо timestamp на строку без мілісекунд
+                    if isinstance(value, datetime):
+                        value = value.strftime("%Y-%m-%d %H:%M:%S")
+                    else:
+                        # Якщо timestamp представлений як строка, парсимо і форматуюємо
+                        value = datetime.fromisoformat(value).strftime("%Y-%m-%d %H:%M:%S")
                 model.setItem(row, col, QStandardItem(str(value) if value is not None else ""))
 
         return model
