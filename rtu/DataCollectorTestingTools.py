4c38159977d095264ca4ec31b46f245130bd9060
vladem255 <vladem255@gmail.com>
2025-02-21 15:05:14 +0200

    feat: testing tools now have emulating of power consumption

--- rtu/DataCollectorTestingTools.py
+++ rtu/DataCollectorTestingTools.py
@@ -1,13 +1,18 @@
 import random
 
 
-def get_test_data(device_model):
+def get_test_data(device_model, last_data):
     if device_model == "SDM120":
+        if last_data is None:
+            last_kwh = 0.0
+        else:
+            last_kwh = last_data.total_kWh_1 if last_data.total_kWh_1 else 0.0
+            last_kwh += random.uniform(0.1, 0.5)
         return {
             "line_voltage_1": random.randrange(200, 240),
             "current_1": random.randrange(1, 20),
             "power_1": 0.22,
-            "total_active_energy": 0.0,
+            "total_active_energy": last_kwh,
             "total_reactive_energy": 0.0,
 
             "active_power_1": 0.22,
@@ -18,6 +23,18 @@ def get_test_data(device_model):
             "export_active_energy_1": 0.0,
         }
     elif device_model == "SDM630":
+        if last_data is None:
+            last_kwh_1 = 0.0
+            last_kwh_2 = 0.0
+            last_kwh_3 = 0.0
+        else:
+            last_kwh_1 = last_data.total_kWh_1 if last_data.total_kWh_1 else 0.0
+            last_kwh_1 += random.uniform(0.1, 0.5)
+            last_kwh_2 = last_data.total_kWh_2 if last_data.total_kWh_2 else 0.0
+            last_kwh_2 += random.uniform(0.1, 0.5)
+            last_kwh_3 = last_data.total_kWh_3 if last_data.total_kWh_3 else 0.0
+            last_kwh_3 += random.uniform(0.1, 0.5)
+        last_kwh = last_kwh_1 + last_kwh_2 + last_kwh_3
         return {
             "line_voltage_1": random.randrange(200, 240),
             "line_voltage_2": random.randrange(200, 240),
@@ -28,10 +45,10 @@ def get_test_data(device_model):
             "power_1": 0.22,
             "power_2": 0.22,
             "power_3": 0.22,
-            "total_kWh_1": 0.0,
-            "total_kWh_2": 0.0,
-            "total_kWh_3": 0.0,
-            "total_kWh": 0.0,
+            "total_kWh_1": last_kwh_1,
+            "total_kWh_2": last_kwh_2,
+            "total_kWh_3": last_kwh_3,
+            "total_kWh": last_kwh,
 
             "power_factor_1": 1,
             "power_factor_2": 1,
@@ -81,6 +98,11 @@ def get_test_data(device_model):
 
         }
     elif device_model == "SDM72D":
+        if last_data is None:
+            last_kwh = 0.0
+        else:
+            last_kwh = last_data.total_kWh if last_data.total_kWh else 0.0
+            last_kwh += random.uniform(0.1, 0.5)
         return {
             "line_voltage_1": random.randrange(200, 240),
             "line_voltage_2": random.randrange(200, 240),
@@ -91,7 +113,7 @@ def get_test_data(device_model):
             "power_1": 0.22,
             "power_2": 0.22,
             "power_3": 0.22,
-            "total_kWh": 0.0,
+            "total_kWh": last_kwh,
 
             "active_power_1": 0.22,
             "active_power_2": 0.22,
