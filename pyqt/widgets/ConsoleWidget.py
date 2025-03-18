23a25234c120cc8f42f18dd603fc7749d90ad294
vladem <vladem255@gmail.com>
2025-03-18 16:12:17 +0200

    feat: add colors to console

--- pyqt/widgets/ConsoleWidget.py
+++ pyqt/widgets/ConsoleWidget.py
@@ -1,17 +1,36 @@
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor, QTextCursor, QTextCharFormat
 from PySide6.QtWidgets import QPlainTextEdit
-
 
 class ConsoleWidget(QPlainTextEdit):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setReadOnly(True)
-        self.document().setMaximumBlockCount(100)  # Обмеження кількості рядків
+        self.document().setMaximumBlockCount(100)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
 
     def write(self, text):
-        self.insertPlainText(text)
+        cursor = self.textCursor()
+        cursor.movePosition(QTextCursor.MoveOperation.End)
+
+        if "WARNING" in text:
+            color = QColor("yellow")
+        elif "ERROR" in text:
+            color = QColor("red")
+        elif "INFO" in text:
+            color = QColor("lime")
+        elif "DEBUG" in text:
+            color = QColor("skyblue")
+        else:
+            color = QColor("white")
+
+        format = QTextCharFormat()
+        format.setForeground(color)
+        cursor.mergeCharFormat(format)
+        cursor.insertText(text)
+
+        self.setTextCursor(cursor)
         self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
 
     def flush(self):
