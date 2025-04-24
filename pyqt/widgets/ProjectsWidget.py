08c06bc8f5c793bdc7781f3c07fbc0e47ae337dc
vladem <vladem255@gmail.com>
2025-04-24 16:24:54 +0300

    try: 7

--- pyqt/widgets/ProjectsWidget.py
+++ pyqt/widgets/ProjectsWidget.py
@@ -102,7 +102,7 @@ class ProjectsWidget(QWidget):
                 ports.append('-Nothing-')
                 port_combo = QComboBox()
                 port_combo.addItems(ports)
-                port_combo.setCurrentText(project.port)
+                #port_combo.setCurrentText(project.port)
                 port_combo.setStyleSheet("font-size: 18px;")
                 port_combo.currentIndexChanged.connect(
                     lambda _, p=project, combo=port_combo:
