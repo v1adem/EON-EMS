12de859de38a9694ed331883111bc3e53bf7c36e
vladem <vladem255@gmail.com>
2025-04-24 16:37:49 +0300

    try: 12

--- pyqt/widgets/ProjectsWidget.py
+++ pyqt/widgets/ProjectsWidget.py
@@ -102,7 +102,7 @@ class ProjectsWidget(QWidget):
                 ports.append('-Nothing-')
                 port_combo = QComboBox()
                 port_combo.addItems(ports)
-                port_combo.setCurrentText(project.port)
+                #port_combo.setCurrentText(project.port)
                 port_combo.setStyleSheet("font-size: 18px;")
                 port_combo.currentIndexChanged.connect(
                     lambda _, p=project, combo=port_combo:
@@ -189,6 +189,7 @@ class ProjectsWidget(QWidget):
                     return
 
                 self.new_project = Project(name=project_name)
+                print(self.new_project) ###########
                 await self.new_project.save()
 
                 self.thread_manager.add_thread(self.new_project, self.main_window)
