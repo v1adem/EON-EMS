1ff183dc87d5db34c4b2618eba202d408164a53c
vladem255 <vladem255@gmail.com>
2025-04-25 13:01:51 +0300

    fix: retry fix

--- rtu/SerialReaderRS485.py
+++ rtu/SerialReaderRS485.py
@@ -71,7 +71,7 @@ class SerialReaderRS485:
 
         self.client = ModbusSerialClient(
             port=self.port, baudrate=baudrate, parity=parity,
-            stopbits=stopbits, bytesize=bytesize, timeout=1, retries=3
+            stopbits=stopbits, bytesize=bytesize, timeout=0.5, retries=1
         )
 
     def connect(self):
