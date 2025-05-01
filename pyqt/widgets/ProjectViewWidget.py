0a4d1adfd8b457585c7f54e4f50f34a34b0603e2
vladem <vladem255@gmail.com>
2025-05-01 11:03:19 +0300

    fix: minor fix 3

--- pyqt/widgets/ProjectViewWidget.py
+++ pyqt/widgets/ProjectViewWidget.py
@@ -188,7 +188,7 @@ class ProjectViewWidget(QWidget):
             wait_time_local = now_utc - timedelta(seconds=600)
             device.wait_time = wait_time_local.astimezone(tz)
             await device.save(update_fields=['wait_time'])
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(1)
             await self.load_devices()
         except Exception as e:
             logger.error(e)
