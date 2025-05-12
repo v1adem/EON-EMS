32f1884a7fea063c75a4d3f6ac61df803659b994
vladem <vladem255@gmail.com>
2025-05-12 14:31:01 +0300

    fix: only 1 console widget 2

--- pyqt/widgets/DeviceDetailsWidget.py
+++ pyqt/widgets/DeviceDetailsWidget.py
@@ -29,11 +29,6 @@ class DeviceDetailsWidget(QWidget):
         self.device_model = self.device.model
         self.main_window = main_window
 
-        self.console_widget = ConsoleWidget()
-        self.output_duplicator = ConsoleOutputDuplicator(self.console_widget, sys.__stdout__)
-        sys.stdout = self.output_duplicator
-        logger.info("Console initialized")
-
         self.report_data = None
 
         self.init_column_labels()
@@ -181,7 +176,8 @@ class DeviceDetailsWidget(QWidget):
 
         bottom_left_layout = QGridLayout()
 
-        bottom_left_layout.addWidget(self.console_widget, 0, 0, 2, 1)
+        console_widget = ConsoleWidget()
+        bottom_left_layout.addWidget(console_widget, 0, 0, 2, 1)
 
         clock_title = QLabel("Поточний час")
         clock_title.setStyleSheet("font-size: 16pt; font-weight: bold;")
@@ -250,7 +246,8 @@ class DeviceDetailsWidget(QWidget):
             "current_lcd": current_lcd,
             "power_lcd": power_lcd,
             "energy_lcd": energy_lcd,
-            "clock_label": clock_label
+            "clock_label": clock_label,
+            "console_widget": console_widget,
         }
 
         voltage_plot_item = voltage_graph.plot([], [], pen=pg.mkPen(color=(0, 102, 204), width=2),
@@ -276,7 +273,7 @@ class DeviceDetailsWidget(QWidget):
 
         self.tabs.addTab(tab, phase_name)
 
-        sys.stdout = ConsoleOutputDuplicator(self.console_widget, sys.__stdout__)
+        sys.stdout = ConsoleOutputDuplicator(console_widget, sys.__stdout__)
         logger.info("Console initialized")
 
     def create_phase_tab_sdm72(self, phase_name):
@@ -317,7 +314,8 @@ class DeviceDetailsWidget(QWidget):
 
         bottom_left_layout = QGridLayout()
 
-        bottom_left_layout.addWidget(self.console_widget, 0, 0, 2, 1)
+        console_widget = ConsoleWidget()
+        bottom_left_layout.addWidget(console_widget, 0, 0, 2, 1)
 
         clock_title = QLabel("Поточний час")
         clock_title.setStyleSheet("font-size: 16pt; font-weight: bold;")
@@ -386,7 +384,8 @@ class DeviceDetailsWidget(QWidget):
             "current_lcd": current_lcd,
             "power_lcd": power_lcd,
             "energy_lcd": energy_lcd,
-            "clock_label": clock_label
+            "clock_label": clock_label,
+            "console_widget": console_widget,
         }
 
         voltage_plot_item = voltage_graph.plot([], [], pen=pg.mkPen(color=(0, 102, 204), width=2),
@@ -411,6 +410,9 @@ class DeviceDetailsWidget(QWidget):
         setattr(self, f"power_scatter_item_{phase_name}", power_scatter_item)
 
         self.tabs.addTab(tab, phase_name)
+
+        sys.stdout = ConsoleOutputDuplicator(console_widget, sys.__stdout__)
+        logger.info("Console initialized")
 
 
     def auto_update(self):
