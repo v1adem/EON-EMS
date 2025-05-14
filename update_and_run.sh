d3bfad531bc1d34851202213d70ef80a69d36f70
vladem <vladem255@gmail.com>
2025-05-14 11:01:20 +0300

    feat: auto creating venv if it is missing / wait for click before closing console

--- update_and_run.sh
+++ update_and_run.sh
@@ -5,13 +5,29 @@ cd /home/admin/Desktop/EON-EMS
 if pgrep -f EON_EMS.py > /dev/null
 then
   echo "Program is running. Before updating - Shut it down"
+  echo "Press Enter to continue..."
+  read
   exit 1
 fi
 
 git pull
 
+if [ ! -d "venv" ]; then
+  echo "Virtual environment 'venv' not found. Creating..."
+  python3 -m venv venv
+  if [ $? -ne 0 ]; then
+    echo "Error creating virtual environment. Exiting."
+    echo "Press Enter to continue..."
+    read
+    exit 1
+  fi
+else
+  echo "Virtual environment 'venv' found."
+fi
+
 source venv/bin/activate
 
+pip install --upgrade pip
 pip install -r req.txt
 
 python3 EON_EMS.py
